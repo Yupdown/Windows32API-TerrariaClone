eceaea58bde2b3826ad9fec7a4d1a06f30c96161
Yup <wts1597@gmail.com>
2023-06-05 23:55:49 +0900

    Jun 5 Commit for merge

--- Terraria-Replica/CEyeMonster.cpp
+++ Terraria-Replica/CEyeMonster.cpp
@@ -12,7 +12,7 @@ CEyeMonster::CEyeMonster(TRWorld* const _trWorld, wstring_view _wstrMonName, wst
 	:CMonster{ _trWorld,_wstrMonName,_wstrMonImgName }
 {
 	auto pAnim = GetComp<CAnimator>();
-	pAnim->CreateAnimation(GetName() + L"Walk", _wstrMonImgName, Vec2{ 0,0 }, Vec2{ 18,12 }, Vec2{ 0,12 }, 0.1f, 2);
+	pAnim->CreateAnimation(GetName() + L"Walk", _wstrMonImgName, Vec2{ 0,0 }, Vec2{ 19,11 }, Vec2{ 0,12 }, 0.1f, 2);
 	pAnim->Play(GetName() + L"Walk", true);
 	auto pRigid = GetComp<CRigidBody>();
 
@@ -34,7 +34,7 @@ void CEyeMonster::render(HDC _dc) const
 	auto vCurPos = GetPos();
 	auto pAnim = GetComp<CAnimator>();
 	auto vDir = (vPlayerPos - vCurPos).Normalize();
-	float fDeg = pAnim->GetAnimDir() == 0 ? acosf(-1 * vDir.x) : acosf(1 * vDir.x);
+	float fDeg = pAnim->GetAnimDir() == 0 ? atan2f(-vDir.y, -vDir.x) : atan2f(-vDir.y, -vDir.x) + F_PI;
 	const auto [vLT, vScale] = Mgr(CCamera)->GetRenderPos(this);
 	Mgr(CCore)->RotateTransform(_dc, fDeg * F_RAD2DEG, vLT + vScale/2.f);
 	CMonster::render(_dc);
